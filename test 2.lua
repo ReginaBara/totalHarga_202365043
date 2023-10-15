fb7a781a19b40c139f29786bd247b7a74df9dc9e
ReginaBara <147903891+ReginaBara@users.noreply.github.com>
2023-10-15 19:43:39 +0800

    test 2

---
+++ test 2.lua
@@ -0,0 +1,27 @@
+String: sabun,samphop;
+integer: hargaSabun, hargaSampho, totalHarga;
+
+sabun="lux"
+sanpho="ricoise"
+hargaSabun,hargaSampho,totalHarga=0;
+
+if(sabun=="lux") then
+   hargaSabun=5000;
+end if
+else if(sabun=="lafboy") then
+   hargaSabun=4000;
+else if(sabun=="sinzui") then
+   hargaSabun=3000;
+end if 
+
+if(shampo=="recoise") then
+   hargaSampho=15000;
+end if
+else if(shampo=="sunslik") then
+   hargaSampho=13000;
+end if
+else if (shampo=="treshme") then
+   hargaSampho=1000;
+end if
+
+print("totalHarga") 
